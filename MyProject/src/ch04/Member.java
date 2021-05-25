759dbbf28f11b1f06b9b623f1b965fe7dd9afc98
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-05-25 16:08:11 +0900

    ch04
    Member update

---
+++ MyProject/src/ch04/Member.java
@@ -0,0 +1,43 @@
+package ch04;
+
+import java.util.*;
+
+public class Member{	
+	public void fluCheck(int year) {
+		int age = Calendar.getInstance().get(Calendar.YEAR)+1-year;
+		if (age<15 || age>=65) {
+			System.out.println("무료예방접종이 가능합니다.");
+			System.out.println();
+		}
+		else {
+			System.out.println("무료접종 대상이 아닙니다.");
+			System.out.println();
+		}
+	}
+	public void healthCheck(int year) {
+		int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+		int age = thisYear+1-year;
+		if(age>=20){
+			if(age%2==0 && thisYear%2==0) {
+				System.out.println("올해 건강검진 대상자입니다.");
+				if(age>=40) {
+					System.out.println("추가로 암검사도 무료로 가능합니다.");
+				}
+			}
+		}
+		else {
+			System.out.println("올해 건강검진 대상자가 아닙니다.");
+		}
+	}
+	public static void main(String[] args) {		
+		Member member = new Member();
+		System.out.printf("태어난 년도를 입력해주세요 : ");
+		Scanner s = new Scanner(System.in);
+		int input = s.nextInt();
+		System.out.println("------------------------------");
+		
+		member.fluCheck(input);
+		member.healthCheck(input);
+	}
+
+}
