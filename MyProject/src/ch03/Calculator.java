81f5c08fbf8706f2802d940f3b3ddab0b9013219
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-05-25 14:57:59 +0900

    ch03
    Calculator

--- MyProject/src/ch03/Calculator.java
+++ MyProject/src/ch03/Calculator.java
@@ -25,21 +25,25 @@ public class Calculator {
 		
 		Calculator cal = new Calculator();
 		
+		System.out.printf("첫번째 숫자를 입력해주세요 : ");
 		Scanner sc1 = new Scanner(System.in);
 		int num1 = sc1.nextInt();
+		System.out.printf("두번째 숫자를 입력해주세요 : ");
 		Scanner sc2 = new Scanner(System.in);
 		int num2 = sc2.nextInt();
 		
-		System.out.println(cal.add(num1, num2));
-		System.out.println(cal.sub(num1, num2));
-		System.out.println(cal.mul(num2, num2));
-		System.out.println(cal.div(num1, num2));
+		System.out.println("두 수의 합 : " + cal.add(num1, num2));
+		System.out.println("두 수의 차 : " + cal.sub(num1, num2));
+		System.out.println("두 수의 곱 : " + cal.mul(num2, num2));
+		System.out.println("두 수의 나눔 : " + cal.div(num1, num2));
 		
+		System.out.println("------------------------");
+		System.out.printf("두번째 숫자를 입력해주세요 : ");
 		Scanner s = new Scanner(System.in);
 		double input = s.nextDouble();
 		
-		System.out.printf("%.6f\n",cal.area(input));
-		System.out.printf("%.6f\n",cal.length(input));
+		System.out.printf("원의 둘레 : %.6f\n",cal.length(input));
+		System.out.printf("원의 넓이 : %.6f\n",cal.area(input));
 	}
 	
 }	
