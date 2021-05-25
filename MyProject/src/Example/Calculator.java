414fc0e18119ed3964cb9b212a6697680533bdc9
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-05-25 10:21:45 +0900

    Calculator
    Calculator Example

---
+++ MyProject/src/Example/Calculator.java
@@ -0,0 +1,16 @@
+package Example;
+
+public class Calculator {
+	public void add(int a, int b) {
+		System.out.println("더하기 : " + (a+b));
+	}
+	public void sub(int a, int b) {
+		System.out.println("빼기 : " + (a-b));
+	}
+	public void mul(int a, int b) {
+		System.out.println("곱하기 : " + (a*b));
+	}
+	public void div(int a, int b) {
+		System.out.println("나누기 : " + (a/b));
+	}
+}
