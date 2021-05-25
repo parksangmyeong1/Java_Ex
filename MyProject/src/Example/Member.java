7f286b80d89b78259a7a710127b9321b670fc71b
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-05-25 10:35:19 +0900

    Member
    update

--- MyProject/src/Example/Member.java
+++ MyProject/src/Example/Member.java
@@ -1,9 +1,29 @@
 package Example;
 
 public class Member {
-
+	String name;
+	int age;
+	double height;
+	boolean hasBook;
+	
+	public Member(String name, int age, double height, boolean hasBook) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.hasBook =  hasBook;
+	}
+	public void Info() {
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("키 : " + height);
+		System.out.println("책 보유 : " + hasBook);
+	}
 	public static void main(String[] args) {
-		System.out.println("박상명입니다.");
+//		System.out.println("박상명입니다.");
+		
+		Member member = new Member("박상명",20,160.0,true);
+		
+		member.Info();
 	}
 
 }
---
+++ MyProject/src/Example/Person.java
@@ -0,0 +1,24 @@
+package Example;
+
+public class Person {
+	String name;
+	String number;
+	String idNo;
+	
+	public Person(String name, String number, String idNo) {
+		this.name = name;
+		this.number = number;
+		this.idNo = idNo;
+	}
+	public void Info() {
+		System.out.println("이름 : " + name);
+		System.out.println("전화번호 : " + number);
+		System.out.println("주민등록번호 : " + idNo);
+	}
+	public static void main(String[] args) {
+		Person person = new Person("박상명","010-1234-5678","210525-112233445");
+		
+		person.Info();
+	}
+
+}
