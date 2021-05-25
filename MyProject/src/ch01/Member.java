8ad7dcb95d9c52fb39bdd26ea107682241c30f9a
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-05-25 10:47:43 +0900

    Member
    Member update

--- MyProject/src/ch01/Member.java
+++ MyProject/src/ch01/Member.java
@@ -18,9 +18,7 @@ public class Member {
 		System.out.println("키 : " + height);
 		System.out.println("책 보유 : " + hasBook);
 	}
-	public static void main(String[] args) {
-//		System.out.println("박상명입니다.");
-		
+	public static void main(String[] args) {		
 		Member member = new Member("박상명",20,160.0,true);
 		
 		member.Info();
