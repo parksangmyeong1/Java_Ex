0ff37a260efcfeea84db5398ae2e33b5a376d528
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-06-03 17:34:59 +0900

    ch7
    Male

---
+++ MyProject/src/ch7/Male.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class Male extends Person {
+
+	public Male(String name, String personNumber) {
+		super(name, personNumber);
+	}
+	@Override
+	public void sayHello() {
+		super.sayHello();
+		System.out.println("나는 남자입니다.");
+	}
+}
