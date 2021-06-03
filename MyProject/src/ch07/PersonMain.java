10a60f602916185956b3954bce2cf2229af45439
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-06-03 17:38:11 +0900

    ch07
    PersonMain

--- MyProject/src/ch07/PersonMain.java
+++ MyProject/src/ch07/PersonMain.java
@@ -5,7 +5,7 @@ public class PersonMain {
 	public static void main(String[] args) {
 		
 		Person person1 = new Male("손흥민", "123456-1234567");
-		Person person2 = new Female("박나래", "000000-2222222");
+		Person person2 = new Female("이지은", "000000-2222222");
 		
 		person1.sayHello();
 		
