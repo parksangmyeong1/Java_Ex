1dba815591d4aa4ff33fa4f8ccc5737e1f5c5d4a
parksangmyeong1 <84554105+parksangmyeong1@users.noreply.github.com>
2021-06-10 17:44:59 +0900

    Project
    Project

--- MyProject/src/javaproject/SmartPhone.java
+++ MyProject/src/javaproject/SmartPhone.java
@@ -7,23 +7,22 @@ import java.util.Scanner;
 public class SmartPhone {
     String name, phone, email, address, birth, group;
     
-    List contacts = new ArrayList();
+    List<Contact> contacts = new ArrayList<Contact>();
     
     void addContact(){
     	Scanner sc = new Scanner(System.in);
-        System.out.println("정보를 저장합니다");
         System.out.println("--------------------");
-        System.out.print("이름을 입력하여 주세요 : ");
+        System.out.print("이름을 입력해주세요 : ");
         name = sc.nextLine();
-        System.out.print("전화번호를 입력하여주세요 : ");
+        System.out.print("전화번호를 입력해주세요 : ");
         phone = sc.nextLine();
-        System.out.print("이메일을 입력하여주세요 : ");
+        System.out.print("이메일을 입력해주세요 : ");
         email = sc.nextLine();
-        System.out.print("주소를 입력하여주세요 : ");
+        System.out.print("주소를 입력해주세요 : ");
         address = sc.nextLine();
-        System.out.print("생일을 입력하여주세요 : ");
+        System.out.print("생일을 입력해주세요 : ");
         birth = sc.nextLine();
-        System.out.print("그룹을 입력하여주세요 : ");
+        System.out.print("그룹을 입력해주세요 : ");
         group = sc.nextLine();
         System.out.println("--------------------");
         
@@ -36,7 +35,7 @@ public class SmartPhone {
         System.out.println("연락처 리스트를 보여드립니다");
         System.out.println("--------------------");
         for (int i = 0; i < contacts.size(); i++) {
-            System.out.println((i+1) + " " + ((Contact)contacts.get(i)).getName());
+            System.out.println((i+1) + " " + contacts.get(i).getName());
         }
         System.out.println("현재 저장된 리스트 : " + contacts.size() + "명");
         System.out.println("--------------------");
@@ -51,7 +50,7 @@ public class SmartPhone {
     	mname = sc.nextLine();
     	
     	for (int i = 0; i < contacts.size(); i++) {
-            if(mname.equals(((Contact) contacts.get(i)).getName())){
+            if(mname.equals(contacts.get(i).getName())){
             	contacts.remove(i);
             }
         }
@@ -66,8 +65,8 @@ public class SmartPhone {
     	mname = sc.nextLine();
     	
     	for (int i = 0; i < contacts.size(); i++) {
-            if(mname.equals(((Contact) contacts.get(i)).getName())){
-            	System.out.println(((Contact) contacts.get(i)).getName() + "님은 " + (i+1) + "에 저장되어있습니다.");
+            if(mname.equals(contacts.get(i).getName())){
+            	System.out.println(contacts.get(i).getName() + "님은 " + (i+1) + "에 저장되어있습니다.");
             }
         }
     }
@@ -79,7 +78,7 @@ public class SmartPhone {
     	mname = sc.nextLine();
     	
     	for (int i = 0; i < contacts.size(); i++) {
-            if(mname.equals(((Contact) contacts.get(i)).getName())){
+            if(mname.equals(contacts.get(i).getName())){
             	
             	System.out.printf("수정할 목록을 입력해주세요 : ");
             	System.out.println("1. 이름  2. 전화번호  3. 이메일  4. 주소  5. 생일  6. 그룹");
@@ -89,33 +88,33 @@ public class SmartPhone {
             	switch(input) {
             	case 1 : 
                 	name = sc.nextLine();
-                	((Contact) contacts.get(i)).setName(name);
-                	System.out.println(((Contact) contacts.get(i)).getName());
+                	contacts.get(i).setName(name);
+                	System.out.println(contacts.get(i).getName());
                 	break;
             	case 2 : 
             		phone = sc.nextLine();
-                	((Contact) contacts.get(i)).setName(phone);
-                	System.out.println(((Contact) contacts.get(i)).getPhone());
+                	contacts.get(i).setName(phone);
+                	System.out.println(contacts.get(i).getPhone());
                 	break;
             	case 3 : 
             		email = sc.nextLine();
-                	((Contact) contacts.get(i)).setName(email);
-                	System.out.println(((Contact) contacts.get(i)).getEmail());
+                	contacts.get(i).setName(email);
+                	System.out.println(contacts.get(i).getEmail());
                 	break;
             	case 4 : 
             		address = sc.nextLine();
-                	((Contact) contacts.get(i)).setName(address);
-                	System.out.println(((Contact) contacts.get(i)).getAddress());
+                	contacts.get(i).setName(address);
+                	System.out.println(contacts.get(i).getAddress());
                 	break;
             	case 5 : 
             		birth = sc.nextLine();
-                	((Contact) contacts.get(i)).setName(birth);
-                	System.out.println(((Contact) contacts.get(i)).getBirth());
+                	contacts.get(i).setName(birth);
+                	System.out.println(contacts.get(i).getBirth());
                 	break;
             	case 6 : 
                 	group = sc.nextLine();
-                	((Contact) contacts.get(i)).setName(group);
-                	System.out.println(((Contact) contacts.get(i)).getGroup());
+                	contacts.get(i).setName(group);
+                	System.out.println(contacts.get(i).getGroup());
                 	break;
                 	
             	}
